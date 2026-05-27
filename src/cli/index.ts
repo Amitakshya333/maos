c6a9dfa9547b9eb2b38285d729a14df1be223e58
Amitakshya Sutar <amitakshyasutar333@gmail.com>
2026-05-27 15:10:38 +0530

    fix: enforce dotenv override: true to prevent stale environment variables from overriding local .env

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-import 'dotenv/config';
+import * as dotenv from 'dotenv';
+dotenv.config({ override: true });
 import { Command } from 'commander';
 import chalk from 'chalk';
 import * as fs from 'fs';
